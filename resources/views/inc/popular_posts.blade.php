5c4b9fa8ff52c72f64d965d353f582bb02a147c9
onlyeffect <onlyeffecttt@yandex.ru>
2018-08-19 14:53:47 +0300

    minor style change

--- resources/views/inc/popular_posts.blade.php
+++ resources/views/inc/popular_posts.blade.php
@@ -1,6 +1,6 @@
 <h3>Popular posts</h3>
 <ul class="list-unstyled">
   @foreach($popularPosts as $post)
-      <li><a href="{{ route('posts.show', $post->id) }}">{{$post->title}}</a> ({{ $post->views }} views)</li>
+      <li style="border-bottom:1px solid #ddd"><a href="{{ route('posts.show', $post->id) }}">{{$post->title}}</a> ({{ $post->views }} views)</li>
   @endforeach
 </ul>
--- resources/views/inc/tags.blade.php
+++ resources/views/inc/tags.blade.php
@@ -2,7 +2,7 @@
 <ul class="list-unstyled">
   @foreach($tags as $tag)
     @if($tag->posts_count > 0)
-      <li><a href="{{ route('posts.index') }}?tag={{ $tag->name }}">#{{$tag->name}}</a> ({{ $tag->posts_count }})</li>
+      <li style="border-bottom:1px solid #ddd"><a href="{{ route('posts.index') }}?tag={{ $tag->name }}">#{{$tag->name}}</a> ({{ $tag->posts_count }})</li>
     @endif
   @endforeach
 </ul>
